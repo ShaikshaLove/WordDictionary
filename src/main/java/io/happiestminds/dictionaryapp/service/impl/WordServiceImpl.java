ac4a9083f2d47a2fbda0d0a0ec43deb5090f847f
ShaikshaLove <44459273+ShaikshaLove@users.noreply.github.com>
2019-09-08 14:15:14 +0530

    Update WordServiceImpl.java

--- src/main/java/io/happiestminds/dictionaryapp/service/impl/WordServiceImpl.java
+++ src/main/java/io/happiestminds/dictionaryapp/service/impl/WordServiceImpl.java
@@ -48,12 +48,14 @@ public class WordServiceImpl implements IWordService {
             while ((line = reader.readLine()) != null) {
                 String[] words = line.split(" ");
                 for(String word:words){
+
                     String lowerCase=word.toLowerCase().trim();
                     // Checking the word is existed in the db or not
+                    //Word fetchWord=wordRepository.findWordByName(lowerCase);
                     if(!isWordAvailble(lowerCase))
                         //saving the word in db only if the word is not available in the DB
                      wordRepository.save(new Word(lowerCase));
                 }
-            }
+           }
     }
 }
